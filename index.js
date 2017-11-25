fa76fa44f9c9f070b87d672638ca584adea30563
13091987Y <atticot.yohann@gmail.com>
2017-11-25 09:33:04 +0100

    Authentification with Google and Facebook

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const FacebookStrategy = require('passport-facebook').Strategy;
+const keys = require('./config/keys');
 const app = express();
 
-app.get("/", (req, res)=>{
-    res.send({hello: 'Yohann'});
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken)=>{ console.log("Success code : ", accessToken); }));
+
+app.get('/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }) 
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google'),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/');
+      }
+);
+
+passport.use(new FacebookStrategy({
+    clientID: keys.facebookClientID,
+    clientSecret: keys.facebookClientSecret,
+    callbackURL: "/auth/facebook/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log("Success code FACEBOOK : ", accessToken);
+    // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+    //   return cb(err, user);
+    // });
+  }
+));
+
+app.get('/auth/facebook',
+passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+passport.authenticate('facebook', { failureRedirect: '/login' }),
+function(req, res) {
+  // Successful authentication, redirect home.
+  res.redirect('/');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
