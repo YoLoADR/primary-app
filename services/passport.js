3af359a3d8789e865dcabc9c6878a708fea52338
13091987Y <atticot.yohann@gmail.com>
2017-11-26 16:13:29 +0100

    Video from 35 to 36 Saving instance model

--- services/passport.js
+++ services/passport.js
@@ -2,21 +2,39 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const FacebookStrategy = require('passport-facebook').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('user');
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-}, (accessToken)=>{ console.log("Success code : ", accessToken); }));
+}, 
+function(accessToken, refreshToken, profile, cb) {
+  console.log("Success code Google accessToken : ", accessToken);
+  console.log("Success code Google profile : ", profile);
+  let user = new User({googleId: profile.id, displayName: profile.displayName}).save(()=>{
+    console.log(user, "save from google");
+  });
+  // User.findOrCreate({facebookId: profile.id, displayName: profile.displayName}, function (err, user) {
+  //   return cb(err, user);
+  // });
+}));
 
 
 passport.use(new FacebookStrategy({
     clientID: keys.facebookClientID,
     clientSecret: keys.facebookClientSecret,
-    callbackURL: "/auth/facebook/callback"
+    callbackURL: "http://localhost:5000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log("Success code FACEBOOK : ", accessToken);
+    console.log("Success code FACEBOOK profile: ", profile);
+    let user = new User({facebookId: profile.id, displayName: profile.displayName});
+    user.save(()=>{
+      console.log(user, "save from facebook");
+    });
     // User.findOrCreate({ facebookId: profile.id }, function (err, user) {
     //   return cb(err, user);
     // });
